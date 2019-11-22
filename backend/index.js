dc38ba3a7d22dcc261fff6f5b8574f7899d1a363
isaiah <isaiahballah@gmail.com>
2019-11-22 10:47:11 -0500

    init backend

---
+++ backend/index.js
@@ -0,0 +1,30 @@
+//require modules
+const mongoose = require('mongoose');
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const morgan = require("morgan");
+const cors = require('cors');
+const dotenv = require('dotenv');
+const cookieParser = require('cookie-parser')
+
+//use modules
+dotenv.config();
+app.use(cors({origin: '*'}));
+app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(cookieParser());
+
+//connect to db
+const mongoURL = "mongodb+srv://isaiah:1qa2ws@development-zd17r.mongodb.net/test?retryWrites=true&w=majority";
+mongoose.connect(mongoURL, {useNewUrlParser:true, useUnifiedTopology: true});
+var db = mongoose.connection;
+db.once('open', () => console.log("Connected to mongo database"));
+db.on('error', console.error.bind(console, "MongoDB connection error: "));
+
+//open web server
+const PORT = process.env.PORT || 9001;
+app.listen(PORT, function() {
+    console.log("Server is running on @: " + "http://localhost:" + PORT);
+});
