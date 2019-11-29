1436b3e2c639faedbc1376ca8b0d666e9c36378a
isaiah <isaiahballah@gmail.com>
2019-11-29 13:36:31 -0500

    moslty implemented 3rd party auth

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,5 +1,7 @@
 //Modules
 require('dotenv').config();
+const bcrypt = require('bcrypt');
+const passport = require("passport");
 
 //Models
 const User = require("../models/user");
@@ -8,7 +10,7 @@ const User = require("../models/user");
 const testing = process.env.TESTING;
 const testUrl = process.env.TEST_URL;
 const url = process.env.URL;
-const port = process.env.URL;
+const port = process.env.PORT;
 const managerEmail = process.env.MANAGER_EMAIL;
 
 //Helper functions
@@ -18,10 +20,14 @@ const randomNumberGenerator = require("../helper/randomNumberGenerator");
 
 function getVerificationLink(email, code) {
 	var endpoint = "verify?email=" + email + "&code=" + code;
-	if (testing) {
-		return testUrl + ":" + port + "/api/" + endpoint;
-	} 
-	return url + "/api/" + endpoint;
+	var string = "";
+	if (testing == "true") {
+		string = testUrl + ":" + port + "/api/" + endpoint;
+	} else {
+		string = url + "/api/" + endpoint;
+	}
+	console.log(string);
+	return string;
 }
 
 //controller functions
@@ -30,14 +36,48 @@ async function deleteAllUsers(req, res) {
 		await User.deleteMany();
 		return res.send("deleted all users");
 
-	} catch(err) {
+	} catch (err) {
 		return res.status(400).send(err);
 	}
 }
 
-async function signup(req, res) {
 
-	let password = bcrypt.hashSync(req.body.password, bcrypt.genSaltSync(10));
+var GoogleStrategy = require('passport-google-oauth20').Strategy;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET;
+// const callbackURL = process.env.callbackURL;
+
+passport.use(new GoogleStrategy({
+	clientID: GOOGLE_CLIENT_ID,
+	clientSecret: GOOGLE_CLIENT_SECRET,
+	callbackURL: "http://localhost:9001/api/auth/google/callback"
+},
+	async function (accessToken, refreshToken, profile, cb) {
+
+		// console.log(profile);
+		console.log(profile);
+	
+		let user = await User.findOne({email: profile._json.email});
+		if (!user) {
+			user = await User.create({email: profile._json.email, verified: true, manager: email == managerEmail, deactivated: false});
+		}
+		console.log(`user = ${user}`);
+		return cb(null, user);//null, user);
+		// return cb(null, user);
+
+		// User.findOrCreate({ googleId: profile.id }, function (err, user) {
+		// 	console.log(profile);
+		// 	return cb(err, user);
+		// });
+	}
+));
+
+
+
+async function signup(req, res) {
+	let pass = req.body.password;
+	if (!pass) return res.status(400).send("must send password");
+	let password = bcrypt.hashSync(pass, bcrypt.genSaltSync(10));
 	let email = req.body.email;
 	let user;
 	let verificationCode = randomNumberGenerator(25);
@@ -45,16 +85,16 @@ async function signup(req, res) {
 	try {
 		user = await User.findOne({ email });
 		if (user) return res.status(400).send("email already in use");
-		
-		User.create({email, password, verified: false, verificationCode, manager: email == managerEmail, deactivated: false}, function (err, user) {
+
+		User.create({ email, password, verified: false, verificationCode, manager: email == managerEmail, deactivated: false }, function (err, user) {
 			if (err) {
 				console.log(err);
 				return res.status(400).send(err);
 			}
 			let message = `visit link to verify ${getVerificationLink(email, verificationCode)}`;
-			sendEmail()
-			return res.json({message: "account created succesfully", user: user});
-			
+			sendEmail(message, email, "lab5 email verification");
+			return res.json({ message: "account created succesfully", user: user });
+
 		});
 
 	} catch (e) { }
@@ -64,21 +104,26 @@ async function signup(req, res) {
 async function verify(req, res) {
 	let code = req.query.code;
 	let email = req.query.email;
-	console.log(`email is: ${email},  code is: ${code}`);
+	//todo redirect user if i want
 
 	try {
-		user = await User.findOne({email, verificationCode: code });
+		user = await User.findOne({ email, verificationCode: code });
 		if (user) {
 			//generate auth token and set auth cookie so client is authenticated and call other api endpoints
 			var jwt = generateJWT(user);
 			res.cookie('auth', jwt);
+			res.send("verified succesfully");
 		} else {
 			return res.status(404).send("in corect code");
 		}
 
-	} catch(e){
-		
+	} catch (e) {
+		return res.status(404).send(e);
 	}
 
 }
 
+exports.deleteAllUsers = deleteAllUsers;
+exports.verify = verify;
+exports.signup = signup;
+exports.passport = passport;
--- backend/index.js
+++ backend/index.js
@@ -7,14 +7,32 @@ const morgan = require("morgan");
 const cors = require('cors');
 const dotenv = require('dotenv');
 const cookieParser = require('cookie-parser')
+const passport = require("passport");
 
 //use modules
 dotenv.config();
+app.use(require('cookie-parser')());
+
 app.use(cors({origin: '*'}));
 app.use(morgan("dev"));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
+app.use(passport.initialize());
+app.use(passport.session());
+
+var user_cache = {};
+
+passport.serializeUser(function(user, next) {
+	console.log(user);
+  let id = user._id;
+  user_cache[id] = user;
+  next(null, id);
+});
+
+passport.deserializeUser(function(id, next) {
+  next(null, user_cache[id]);
+});
 
 //connect to db
 const mongoURL = process.env.MONGO_URL;
@@ -23,8 +41,22 @@ var db = mongoose.connection;
 db.once('open', () => console.log("Connected to mongo database"));
 db.on('error', console.error.bind(console, "MongoDB connection error: "));
 
+
+//routes
+
+//Routers
+const userRouter = require('./routes/userRouter');
+
+//middleware
+// const authMiddleware = require("./middleware/authMiddleware");
+
+//Routes
+app.use('/api', userRouter);
+app.use('/api/privacy', (req, res) => res.send("cool p"));
+app.use('/api/terms', (req, res) => res.send("cool v"));
+
 //open web server
-const PORT = process.env.PORT || 9001;
+const PORT = process.env.PORT;
 app.listen(PORT, function() {
     console.log("Server is running on @: " + "http://localhost:" + PORT);
 });
